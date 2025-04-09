5fbd4f19431a7a23874903c7ebd58a792112f558
Paul Watkinson <paul.watkinson@pushgaming.com>
2025-04-09 15:33:52 +0100

    Disabled `conform` forceably formatting file on save

--- patches.nix
+++ patches.nix
@@ -54,4 +54,11 @@ in {
         single_file_support = true,
       };
     '';
+
+  # conform is automatically enabled by some formatters, and without setting
+  # these to `null`, formatting is forceably done on save, even if disabled.
+  formatter.conform-nvim.setupOpts = {
+    format_on_save = lib.mkForce null;
+    format_after_save = lib.mkForce null;
+  };
 }
